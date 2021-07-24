0348c0865458ca91b4d5c6dc76a4c57eee4b4b87
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-07-24 16:52:46 +0100

    user data from backend in login, and RegisterComplete plus login with Google

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,83 +1,106 @@
-import React, { useState, useEffect } from "react";
-import { auth } from "../../firebase";
-import { toast } from "react-toastify";
+import React, {useState, useEffect} from "react";
+import {auth} from "../../firebase";
+import {toast} from "react-toastify";
+import {useDispatch, useSelector} from "react-redux";
+import axios from "axios";
 
-const RegisterComplete = ({ history }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import {createOrUpdateUser} from "../../functions/auth";
 
-  useEffect(() => {
-    setEmail(window.localStorage.getItem("emailForRegistration"));
-    // console.log(window.location.href);
-    // console.log(window.localStorage.getItem("emailForRegistration"));
-  }, []);
+const RegisterComplete = ({history}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // validation
-    if (!email || !password) {
-      toast.error("Email and password is required");
-      return;
-    }
+    let dispatch = useDispatch()
+    const {user} = useSelector((state) => ({...state}));
 
-    if (password.length < 6) {
-      toast.error("Password must be at least 6 characters long");
-      return;
-    }
+    useEffect(() => {
+        setEmail(window.localStorage.getItem("emailForRegistration"));
+        // console.log(window.location.href);
+        // console.log(window.localStorage.getItem("emailForRegistration"));
+    }, []);
 
-    try {
-      const result = await auth.signInWithEmailLink(
-        email,
-        window.location.href
-      );
-      //   console.log("RESULT", result);
-      if (result.user.emailVerified) {
-        // remove user email fom local storage
-        window.localStorage.removeItem("emailForRegistration");
-        // get user id token
-        let user = auth.currentUser;
-        await user.updatePassword(password);
-        const idTokenResult = await user.getIdTokenResult();
-        // redux store
-        console.log("user", user, "idTokenResult", idTokenResult);
-        // redirect
-        history.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error(error.message);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // validation
+        if (!email || !password) {
+            toast.error("Email and password is required");
+            return;
+        }
 
-  const completeRegistrationForm = () => (
-    <form onSubmit={handleSubmit}>
-      <input type="email" className="form-control" value={email} disabled />
+        if (password.length < 6) {
+            toast.error("Password must be at least 6 characters long");
+            return;
+        }
 
-      <input
-        type="password"
-        className="form-control"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        autoFocus
-      />
-      <br />
-      <button type="submit" className="btn btn-raised">
-        Complete Registration
-      </button>
-    </form>
-  );
+        try {
+            const result = await auth.signInWithEmailLink(
+                email,
+                window.location.href
+            );
+            if (result.user.emailVerified) {
+                // remove user email fom local storage
+                window.localStorage.removeItem("emailForRegistration");
+                // get user id token
+                let user = auth.currentUser;
+                await user.updatePassword(password);
+                const idTokenResult = await user.getIdTokenResult();
 
-  return (
-    <div className="container p-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <h4>Register Complete</h4>
-          {completeRegistrationForm()}
+                createOrUpdateUser(idTokenResult.token)
+                    .then(({data}) => {
+                        console.log('DATA: ', data)
+                        dispatch({
+                            type: "LOGGED_IN_USER",
+                            payload: {
+                                email: data.email,
+                                name: data.name,
+                                role: data.role,
+                                _id: data._id,
+                                token: idTokenResult.token,
+                            },
+                        });
+
+                    })
+                    .catch(err => {
+                        console.error(err)
+                        toast.error(err.message)
+                    });
+                history.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
+
+    const completeRegistrationForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="email" className="form-control" value={email} disabled/>
+
+            <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                autoFocus
+            />
+            <br/>
+            <button type="submit" className="btn btn-raised">
+                Complete Registration
+            </button>
+        </form>
+    );
+
+    return (
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register Complete</h4>
+                    {completeRegistrationForm()}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RegisterComplete;
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,16 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers); // token
-  next();
+exports.authCheck = async (req, res, next) => {
+  try{
+    console.log("VERIFYING FIREBASE USER")
+    const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+    console.log('FIREBASE USER: ', firebaseUser)
+    req.user = firebaseUser;
+    next()
+  } catch (err){
+    console.log(err)
+    res.status(401).json({
+      err: "Invalid or expired token"
+    })
+  }
 };
