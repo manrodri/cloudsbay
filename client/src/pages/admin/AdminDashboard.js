7cf5581c7aa78a894d73ad22dc403719d6cd4696
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-07-25 19:39:53 +0100

    added adminCheck and new /current-admin route

---
+++ client/src/pages/admin/AdminDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminDashboard = () => {
+    return (
+        <React.Fragment>
+            <p>Admin dashboard</p>
+        </React.Fragment>
+    )
+}
+
+export default AdminDashboard;
