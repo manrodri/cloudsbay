5407fbbc5d0235742dc1ddf3a2febe844ec32420
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-07-26 09:21:59 +0100

    added adminCheck and new /current-admin route

---
+++ client/src/components/routes/AdminRoute.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import {Route} from "react-router-dom";
+import {useSelector} from "react-redux";
+import LoadingToRedirect from "./LoadingToRedirect";
+import {currentAdmin} from "../../functions/auth";
+
+const AdminRoute = ({children, ...rest}) => {
+
+    const [ok, setOk] = useState(false)
+    const {user} = useSelector((state) => ({...state}));
+
+    useEffect(()=> {
+        if(user && user.token){
+            currentAdmin(user.token)
+                .then((res)=>{
+                    console.log('CURRENT ADMIN RES:', res)
+                    setOk(true)
+                })
+                .catch((err)=> {
+                    setOk(false)
+                    console.log('ADMIN ROUTE ERROR: ', err)
+                })
+        }
+    }, [user])
+
+    return ok  ? (
+        <Route {...rest} />
+    ) : (
+        <LoadingToRedirect/>
+    );
+};
+
+export default AdminRoute;
